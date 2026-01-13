05a8af7f129d183637c7ab5fe84d50a2aaa16455
ashl777 <81948916+ashl777@users.noreply.github.com>
2026-01-13 11:28:15 +0800

    Add Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'GLAND'
+copyright = '2024, kai'
+author = 'kai'
+
+# 扩展：启用 Markdown 支持和 RTD 主题
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+html_theme = 'sphinx_rtd_theme'
